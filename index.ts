25122c2b10b1c9ba1413b0a6a4870db69e4462a3
shenguotao <shenguotao@taptap.com>
2021-08-08 08:50:32 +0800

    add regexpress module.

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import getDecimalDigits from './src/get-decimal-digits';
 import isInteger from './src/is-integer';
-
+import regexp from './src/regexp'
 
 export default {
+    regexp,
     isInteger,
     getDecimalDigits
 }
